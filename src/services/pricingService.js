0657cb7ddd225bc4b072dad9e68bd6b39a1646ef
sakshi-uwo <sakshi@uwo24.com>
2026-07-17 14:56:35 +0530

    just commit

--- src/services/pricingService.js
+++ src/services/pricingService.js
@@ -72,20 +72,3 @@ export const getCreditHistory = async () => {
   return response.data;
 };
 
-export const createPaypalOrder = async (orderData) => {
-  const response = await axios.post(
-    `${API}/payment/paypal/create-order`,
-    orderData,
-    { headers: getAuthHeaders() }
-  );
-  return response.data;
-};
-
-export const capturePaypalOrder = async (orderID, planId, billingCycle, billingDetails = null) => {
-  const response = await axios.post(
-    `${API}/payment/paypal/capture`,
-    { orderID, planId, billingCycle, billingDetails },
-    { headers: getAuthHeaders() }
-  );
-  return response.data;
-};
